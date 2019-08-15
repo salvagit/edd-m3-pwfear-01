0d5d2cdbb9c239de888aba54740df16fd717f504
Salvador Palmiciano <c-spalmiciano@fourwindsinteractive.com>
2019-08-15 20:27:44 -0300

    pasamos input de class a function

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,24 +1,9 @@
-import React, { PureComponent } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class Input extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  componentWillReceiveProps(props) {
-    console.log(
-      `nuevas props :${props.value}`,
-      `viejas props: ${this.props.value}`
-    );
-  }
-
-  render() {
-    const { type, onChange, value } = this.props;
-    return <input type={type} onChange={onChange} value={value} />;
-  }
-}
+const Input = ({ type, onChange, value }) => (
+  <input type={type} onChange={onChange} value={value} />
+);
 
 Input.propTypes = {
   type: PropTypes.string.isRequired,
