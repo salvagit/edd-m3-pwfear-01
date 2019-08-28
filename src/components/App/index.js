f6bfd98d1e9462082c126ebd58c4396efb2e8493
Salvador Palmiciano <c-spalmiciano@fourwindsinteractive.com>
2019-08-27 21:16:24 -0300

    add react-router-dom

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import List from "../../Routes/List";
 
-const App = () => <List />;
+const App = () => (
+  <Router>
+    <Route path="/" exact render={() => "hola root"} />
+    <Route path="/list" component={List} />
+  </Router>
+);
 
 export default App;
